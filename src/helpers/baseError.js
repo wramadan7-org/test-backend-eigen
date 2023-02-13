efc949d36da40f9bbcdc6184e8f7902e0ca4fe32
Wahyu Ramadan <wramadan1203@gmail.com>
2023-02-13 21:33:59 +0700

    create base error

---
+++ src/helpers/baseError.js
@@ -0,0 +1,16 @@
+class BaseError extends Error {
+  constructor(message, statusCode, stack = '', isOperational = true) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+module.exports = BaseError;
