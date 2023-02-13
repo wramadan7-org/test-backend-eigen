75795abd143e1f7ae0a38ae76ed25db36a3b3e5b
Wahyu Ramadan <wramadan1203@gmail.com>
2023-02-13 22:01:49 +0700

    use morgan, logger and handling error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const httpStatus = require('http-status');
+const morgan = require('./configurations/morgan');
+const logger = require('./configurations/logger');
+const BaseError = require('./helpers/baseError');
+const { errorConverter, errorHandler } = require('./middlewares/errorHandler');
 
 const { NODE_PORT } = process.env;
 
@@ -10,6 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use('*', cors());
+app.use(morgan.successHandler);
+app.use(morgan.errorHandler);
 
 // Wrap the response
 app.response.sendWrapped = function (message, data, statusCode = httpStatus.OK) {
@@ -20,8 +27,23 @@ app.response.sendWrapped = function (message, data, statusCode = httpStatus.OK) 
   });
 };
 
-app.listen(NODE_PORT, () => {
-  console.log(`Server run on port ${NODE_PORT}`);
+app.get('/', (req, res) => {
+  const data = 'ramaa';
+
+  if (data !== 'rama') throw new BaseError('Data not rama', httpStatus.CONFLICT);
+
+  res.sendWrapped('Data is Rama', data, httpStatus.OK);
 });
+
+app.listen(NODE_PORT, () => {
+  logger.info(`Server run on port ${NODE_PORT}`);
+});
+
+app.use((req, res, next) => {
+  next(new BaseError('Not found', httpStatus.NOT_FOUND));
+});
+
+app.use(errorConverter);
+app.use(errorHandler);
 
 module.exports = app;
