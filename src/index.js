5e3282ad0e25a28202fd05e45092580622833c9d
Wahyu Ramadan <wramadan1203@gmail.com>
2023-02-13 18:58:06 +0700

    setup default express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('hallo');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const { NODE_PORT } = process.env;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.options('*', cors());
+
+app.listen(NODE_PORT, () => {
+  console.log(`Server start on port ${NODE_PORT}`);
+});
+
+module.exports = app;
