924ba63cd37bae27b436b99abf63a9e267dca033
Wahyu Ramadan <wramadan1203@gmail.com>
2023-02-13 21:39:40 +0700

    wrap response

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const httpStatus = require('http-status');
 
 const { NODE_PORT } = process.env;
 
@@ -9,6 +10,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use('*', cors());
+
+// Wrap the response
+app.response.sendWrapped = function (message, data, statusCode = httpStatus.OK) {
+  return this.status(statusCode).send({
+    status: statusCode,
+    message,
+    data,
+  });
+};
 
 app.listen(NODE_PORT, () => {
   console.log(`Server run on port ${NODE_PORT}`);
