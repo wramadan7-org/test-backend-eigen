78e07a24e447157c433b4d18225cf79be5f0c932
Wahyu Ramadan <wramadan1203@gmail.com>
2023-02-13 21:01:52 +0700

    uninstall typescript

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
@@ -9,10 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.options('*', cors());
+app.use('*', cors());
 
 app.listen(NODE_PORT, () => {
-  console.log(`Server start on port ${NODE_PORT}`);
+  console.log(`Server run on port ${NODE_PORT}`);
 });
 
 module.exports = app;
