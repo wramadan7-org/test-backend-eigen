f92afc016ec0aaeec27f2e027d8a2902e30c1c66
Wahyu Ramadan <wramadan1203@gmail.com>
2023-02-16 23:16:46 +0700

    add field isPenalized in table members

--- src/migrations/20230216063502-create-member.js
+++ src/migrations/20230216063502-create-member.js
@@ -12,6 +12,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      isPenalized: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
--- src/models/member.js
+++ src/models/member.js
@@ -18,6 +18,10 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    isPenalized: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
   }, {
     sequelize,
     modelName: 'Member',
