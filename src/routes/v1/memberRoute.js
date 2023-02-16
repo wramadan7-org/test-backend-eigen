587f3b5da6b0032013ae150a20ba4ca161cfd7f4
Wahyu Ramadan <wramadan1203@gmail.com>
2023-02-16 23:47:51 +0700

    route create member use validation

--- src/routes/v1/memberRoute.js
+++ src/routes/v1/memberRoute.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const { createMemberController } = require('../../controllers/memberController');
+const validate = require('../../middlewares/validator');
+const { createMemberValidation } = require('../../validations/memberValidation');
 
 const router = Router();
 
-router.post('/', createMemberController);
+router.post('/', validate(createMemberValidation), createMemberController);
 
 module.exports = router;
